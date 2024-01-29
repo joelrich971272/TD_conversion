9c507ee0029e2df6641a9bed1d682d060b233094
Joel <sjbjdi.com>
2024-01-29 15:37:10 +0100

    begining

--- src/main/java/fr/btsciel/td_convertisseur/HelloController.java
+++ src/main/java/fr/btsciel/td_convertisseur/HelloController.java
@@ -1,6 +1,7 @@
 package fr.btsciel.td_convertisseur;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -9,7 +10,7 @@ import javafx.scene.control.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class HelloController {
+public class HelloController implements  Initializable{
     @FXML
     private Label dialogW;
     public Button conversion ;
@@ -25,13 +26,18 @@ public class HelloController {
             dialogW.showAndWait() ;
         }
 
-    public void initialize(URL location, ResourceBundle resource) {
-        textField_Dollars.setDisable(true);
-        textField_Euros.setDisable(false);
-    }
+
     final double Taux_E_D = 1.1 ;
     public void conversion(){
         textField_Euros.setText(String.valueOf(Double.parseDouble(textField_Euros + Taux_E_D)));
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        textField_Dollars.setDisable(true);
+        textField_Euros.setDisable(false);
+        conversion.setOnAction(event -> conversion());
+
     }
 }
 

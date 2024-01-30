c82cd0a4df9cd9796a8090b60b30d8a7b130dc13
Joel <sjbjdi.com>
2024-01-30 14:36:37 +0100

    update

--- src/main/java/fr/btsciel/td_convertisseur/HelloController.java
+++ src/main/java/fr/btsciel/td_convertisseur/HelloController.java
@@ -1,13 +1,13 @@
 package fr.btsciel.td_convertisseur;
 
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
+import java.awt.*;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class HelloController implements  Initializable{
@@ -30,6 +30,27 @@ public class HelloController implements  Initializable{
     final double Taux_E_D = 1.1 ;
     public void conversion(){
         textField_Euros.setText(String.valueOf(Double.parseDouble(textField_Euros + Taux_E_D)));
+        private void dollarsEuros(){
+            textField_Euros.setEditable(true);
+            textField_Euros.setDisable(false);
+            textField_Euros.setEditable(false);
+            textField_Euros.setDisable(true);
+        }
+    }
+
+    private void ComboSelection(Event event){
+        (ComboBox)event.getSource();
+
+    }
+    private ArrayList<String> fabriquedonnees(){
+        ArrayList<String> mesPrompt = new ArrayList<>() ;
+        mesPrompt.add("Prompt 1");
+        mesPrompt.add("Prompt 2");
+        mesPrompt.add("Prompt 3");
+        mesPrompt.add("Prompt 4");
+        mesPrompt.add("Prompt 5");
+        mesPrompt.forEach(element -> boxSelection.getItems().add(element));
+        return mesPrompt ;
     }
 
     @Override
@@ -37,6 +58,7 @@ public class HelloController implements  Initializable{
         textField_Dollars.setDisable(true);
         textField_Euros.setDisable(false);
         conversion.setOnAction(event -> conversion());
+
 
     }
 }
